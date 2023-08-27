9c9cc3abb4f3f3c240ec0f731d11d33f540b073c
saneep-pp <saneepprince@gmail.com>
2023-08-27 21:37:23 +0530

    fix some bugs

--- src/App.js
+++ src/App.js
@@ -1,76 +1,75 @@
-import logo from './logo.svg'
-import './App.css'
-import Form from './components/Form'
-import BmiScore from './components/BmiScore'
-import BmiList from './components/BmiList'
-import { useState } from 'react'
+
+import "./App.css";
+import Form from "./components/Form";
+import BmiScore from "./components/BmiScore";
+import BmiList from "./components/BmiList";
+import { useState } from "react";
 
 function App() {
-  const [changeWeight,setChangeWeight]= useState({weight:"",type:""});
-  const [bmi, setBmi] = useState('00');
-  const [bmiType, setBmiType] = useState('Not Calculated');
-  const [bmiRange,setBmiRange]=useState({
-    underWeight:{low:""},
-    normal:{low:"",high:""},
-    overWeight:{low:"",high:""},
-    obesityOne:{low:"",high:""},
-    obesityTwo:{low:"",high:""},
-    obesityThree:{high:""},
+  const [changeWeight, setChangeWeight] = useState({ weight: "", type: "" });
+  const [bmi, setBmi] = useState("00");
+  const [bmiType, setBmiType] = useState("Not Calculated");
+  const [bmiRange, setBmiRange] = useState({
+    underWeight: { low: "" },
+    normal: { low: "", high: "" },
+    overWeight: { low: "", high: "" },
+    obesityOne: { low: "", high: "" },
+    obesityTwo: { low: "", high: "" },
+    obesityThree: { high: "" },
   });
   const onFormSub = (w, h) => {
-    let b = calBmi(w, h)
-    setBmi(b)
+    let b = calBmi(w, h);
+    setBmi(b);
     // let bType = weightType(b)
-    setBmiType(weightType(b))
-    const range={
-      underWeight:{low:calWeight(18.5,h)},
-      normal:{low:calWeight(18.5,h),high:calWeight(24.9,h)},
-      overWeight:{low:calWeight(25,h),high:calWeight(29.9,h)},
-      obesityOne:{low:calWeight(30,h),high:calWeight(34.9,h)},
-      obesityTwo:{low:calWeight(35,h),high:calWeight(39.9,h)},
-      obesityThree:{high:calWeight(40,h)},
+    setBmiType(weightType(b));
+    const range = {
+      underWeight: { low: calWeight(18.5, h) },
+      normal: { low: calWeight(18.5, h), high: calWeight(24.9, h) },
+      overWeight: { low: calWeight(25, h), high: calWeight(29.9, h) },
+      obesityOne: { low: calWeight(30, h), high: calWeight(34.9, h) },
+      obesityTwo: { low: calWeight(35, h), high: calWeight(39.9, h) },
+      obesityThree: { high: calWeight(40, h) },
     };
     setBmiRange(range);
-    setChangeWeight(weightChange(b,w,range));
+    setChangeWeight(weightChange(b, w, range));
   };
   const calBmi = (w, h) => (w / (h * h)).toFixed(2);
-  const calWeight=(b,h)=>(b*h*h).toFixed(2);
-const  weightChange=(b,w,range)=>{
-  let changeObj;
-  if(b>24.9){
-    changeObj={
-      weight:(w-range.normal.high).toFixed(2),
-      type:"positive",
-    };
-    return changeObj;
-  }else if(b<18.5){
-    changeObj={
-      weight:(range.normal.low-w).toFixed(2),
-      type:"negative",
-    };
-    return changeObj;
-  }else{
-    changeObj={weight:0,type:"normal"}
-     
+  const calWeight = (b, h) => (b * h * h).toFixed(2);
+  const weightChange = (b, w, range) => {
+    let changeObj;
+    if (b > 24.9) {
+      changeObj = {
+        weight: (w - range.normal.high).toFixed(2),
+        type: "positive",
+      };
       return changeObj;
-    
-  }
-}
+    } else if (b < 18.5) {
+      changeObj = {
+        weight: (range.normal.low - w).toFixed(2),
+        type: "negative",
+      };
+      return changeObj;
+    } else {
+      changeObj = { weight: 0, type: "normal" };
+
+      return changeObj;
+    }
+  };
   const weightType = (bmi) => {
     if (bmi < 18.4) {
-      return 'UnderWeight';
+      return "UnderWeight";
     } else if (18.5 < bmi && bmi < 24.9) {
-      return 'Normal';
+      return "Normal";
     } else if (24.9 < bmi && bmi < 29.9) {
-      return 'OverWeight';
+      return "OverWeight";
     } else if (29.9 < bmi && bmi < 34.9) {
-      return 'Obesity class I';
+      return "Obesity class I";
     } else if (34.9 < bmi && bmi < 39.9) {
-      return 'Obesity class II';
+      return "Obesity class II";
     } else if (bmi > 39.9) {
-      return 'Obesity class III';
+      return "Obesity class III";
     }
-  }
+  };
 
   //let bmi=18;
   // const changeBmi = () => {
@@ -88,14 +87,14 @@ const  weightChange=(b,w,range)=>{
       </div>
       <div className="row justify-content-center mt-5 mx-2">
         <div className=" col-12 col-sm-6 mb-5">
-          <BmiScore bmiNo={bmi} bmiName={bmiType}changeWeight={changeWeight}/>
+          <BmiScore bmiNo={bmi} bmiName={bmiType} changeWeight={changeWeight} />
         </div>
         <div className="col-12 col-sm-6">
-          <BmiList range={bmiRange} bmi={bmi}/>
+          <BmiList range={bmiRange} bmi={bmi} />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,20 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-function form({ getData }) {
-  const [weight, setWeight] = useState('')
-  const [height, setHeight] = useState('')
-  const [alert, setAlert] = useState(false)
+const Form=({ getData })=> {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [alert, setAlert] = useState(false);
+
   const onSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (isNaN(weight) || isNaN(height)) {
-      setAlert(true)
+      setAlert(true);
     } else {
-      getData(weight, height)
-      setAlert(false)
+      getData(weight, height);
+      setAlert(false);
     }
-  }
-  // let alertMessage
-  // if(alert){
-  // alertMessage=<div className="alert alert-danger" role="alert">Please Enter A valid Input</div>
-  // }
-  // else{
-  // alertMessage="";
-  // }
+  };
+
   return (
     <div className="col-sm-4 shadow rounded px-5">
       <h1 className="text-center pt-3 text-secondary h2">BMI Calculator</h1>
@@ -58,7 +53,7 @@ function form({ getData }) {
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default form
+export default Form;
